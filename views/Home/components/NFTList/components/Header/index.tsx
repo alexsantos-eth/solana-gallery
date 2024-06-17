5290579a0c1925f84435826b6106d8928572f8bd
alexsantos-eth <alexdsantosv@gmail.com>
2024-06-17 08:51:20 -0600

    feat(Dev): :fire: Add not found

--- views/Home/components/NFTList/components/Header/index.tsx
+++ views/Home/components/NFTList/components/Header/index.tsx
@@ -22,8 +22,8 @@ const Header: React.FC<HeaderProps> = ({ length }) => {
 
   return (
     <>
-      <Spacer y={4} />
       <BackgroundBeams />
+      <Spacer y={4} />
 
       <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
